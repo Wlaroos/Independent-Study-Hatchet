27cf364769d520ac0fe9ddebf360d787ce77e5aa
Mikey <mikeybess27@gmail.com>
2022-11-03 10:25:26 -0500

    Added Witch Rig

---
+++ Assets/Scripts/Witch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Witch : EnemyBase
+{
+
+    protected override int ArrowDirection()
+    {
+        return base.ArrowDirection();
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+    }
+}
