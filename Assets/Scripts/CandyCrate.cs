78b4664d491758a32849b73ffbb45c5a5907ba91
Mikey <mikeybess27@gmail.com>
2022-12-08 10:17:28 -0600

    Crate Healthbar, Sign Art Fix

--- Assets/Scripts/CandyCrate.cs
+++ Assets/Scripts/CandyCrate.cs
@@ -7,9 +7,11 @@ public class CandyCrate : MonoBehaviour
     ParticleSystem ps;
     [SerializeField] private int _maxHealth = 100;
     private int _currentHealth;
+    private SpriteRenderer _hpsr;
 
     private void Awake()
     {
+        _hpsr = transform.GetChild(0).GetComponent<SpriteRenderer>();
         ps = GetComponent<ParticleSystem>();
         _currentHealth = _maxHealth;
     }
@@ -17,6 +19,9 @@ public class CandyCrate : MonoBehaviour
     public void TakeDamage(int amount)
     {
         _currentHealth -= amount;
+
+        _hpsr.transform.localScale = new Vector3((float)_currentHealth / (float)_maxHealth * 1.5f, .15f, 1);
+
         Debug.Log("Current Candy:" + _currentHealth);
         ps.Play();
     }
