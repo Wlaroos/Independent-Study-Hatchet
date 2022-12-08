3e6f977c410cb1ecac6e252cdffb5623e1b6c4be
Mikey <mikeybess27@gmail.com>
2022-12-08 04:16:42 -0600

    Parallax Script

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    float _length;
+    float _startPos;
+
+    [SerializeField] Camera _camRef;
+    [SerializeField] float _parallaxAmount;
+
+
+    private void Awake()
+    {
+        _startPos = transform.position.x;
+        _length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float temp = (_camRef.transform.position.x * (1 - _parallaxAmount));
+        float dist = (_camRef.transform.position.x * _parallaxAmount);
+
+        transform.position = new Vector3(_startPos + dist, transform.position.y, transform.position.z);
+
+        if(temp > _startPos + _length)
+        {
+            _startPos += _length;
+        }
+        else if (temp < _startPos - _length)
+        {
+            _startPos -= _length;
+        }
+    }
+}
